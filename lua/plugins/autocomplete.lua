b6c48b8bb6d75e627f954cc4ab70d211939719c4
Ross Pearlstone <ross@pearlstone.us>
2025-12-04 20:15:33 -0500

    updated autocomplete to blink with updated lsp and fzf search

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,59 +1,110 @@
 return {
-  {
-    "saadparwaiz1/cmp_luasnip", -- Snippet source
-    "L3MON4D3/LuaSnip",         -- Snippet engine
-  },
-  {
-    "hrsh7th/nvim-cmp",  -- Autocompletion plugin
-    dependendencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
+    {
+        "saghen/blink.compat",
+        -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+        version = "*",
+        -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+        lazy = true,
+        -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+        opts = {},
     },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require "cmp"
-
-      cmp.setup {
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
+    {
+        "saghen/blink.cmp",
+        -- optional: provides snippets for the snippet source
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+            "moyiz/blink-emoji.nvim",
+            "ray-x/cmp-sql",
         },
-        auto_brackets = {},
-        completion = { completeopt = "menu,menuone,noinsert" },
 
-        mapping = cmp.mapping.preset.insert {
-          -- Select the [n]ext/[p]revious item
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
+        -- use a release tag to download pre-built binaries
+        version = "1.*",
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
 
-          -- Scroll the documentation window [b]ack/[f]orward
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = {
+                preset = "default",
+                ["<C-Z>"] = { "accept", "fallback" },
+            },
 
-          -- Accept ([y]es) or [e]xit the completion.
-          --  This will auto-import if your LSP supports it.
-          --  This will expand snippets if the LSP sent a snippet.
-          ["<CR>"] = cmp.mapping.confirm { select = true },
-          ["<C-e>"] = cmp.mapping.abort(),
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = "mono",
+            },
 
-          -- Manually trigger a completion from nvim-cmp.
-          ["<C-Space>"] = cmp.mapping.complete {},
+            -- (Default) Only show the documentation popup when manually triggered
+            completion = { documentation = { auto_show = true } },
+            signature = { enabled = true },
 
-          -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-          --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer", "emoji", "sql" },
+                providers = {
+                    emoji = {
+                        module = "blink-emoji",
+                        name = "Emoji",
+                        score_offset = 15, -- Tune by preference
+                        opts = { insert = true }, -- Insert emoji (default) or complete its name
+                        should_show_items = function()
+                            return vim.tbl_contains(
+                                -- Enable emoji completion only for git commits and markdown.
+                                -- By default, enabled for all file-types.
+                                { "gitcommit", "markdown" },
+                                vim.o.filetype
+                            )
+                        end,
+                    },
+                    sql = {
+                        -- IMPORTANT: use the same name as you would for nvim-cmp
+                        name = "sql",
+                        module = "blink.compat.source",
+
+                        -- all blink.cmp source config options work as normal:
+                        score_offset = -3,
+
+                        -- this table is passed directly to the proxied completion source
+                        -- as the `option` field in nvim-cmp's source config
+                        --
+                        -- this is NOT the same as the opts in a plugin's lazy.nvim spec
+                        opts = {},
+                        should_show_items = function()
+                            return vim.tbl_contains(
+                                -- Enable emoji completion only for git commits and markdown.
+                                -- By default, enabled for all file-types.
+                                { "sql" },
+                                vim.o.filetype
+                            )
+                        end,
+                    },
+                },
+            },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" },
         },
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "buffer", option = { keyword_length = 3, }, },
-        },
-      }
-
-    end
-  }
+        opts_extend = { "sources.default" },
+    },
 }
