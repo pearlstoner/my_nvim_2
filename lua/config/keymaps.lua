a47e96111da07d6bd1768b51f0ef2f46a15c8fad
Ross Pearlstone <rosspearlstone@Rosss-MacBook-Air.local>
2025-12-11 09:17:38 -0500

    added esc for noh and blink.cmp config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,12 +5,13 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Up, Down
-vim.keymap.set('n', 'j', 'gj', { desc = 'Up', noremap = true})
-vim.keymap.set('n', 'k', 'gk', { desc = 'Down', noremap = true})
+vim.keymap.set("n", "j", "gj", { desc = "Up", noremap = true })
+vim.keymap.set("n", "k", "gk", { desc = "Down", noremap = true })
 vim.keymap.set("n", "<leader>of", "<CMD>Oil --float<CR>", { desc = "Open Oil float" })
 vim.keymap.set("n", "<Tab>", function()
-  vim.cmd(vim.bo.filetype == 'oil' and 'bd' or 'Oil')
+	vim.cmd(vim.bo.filetype == "oil" and "bd" or "Oil")
 end, { desc = "Toggle Oil" })
 -- Define a keymap to open Lazygit
 vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "Open Lazygit" })
 vim.keymap.set("n", "<leader>tt", ":ToggleTerm<CR>", { desc = "Open Terminal" })
+vim.api.nvim_set_keymap("n", "<esc>", ":nohlsearch<CR>", { noremap = true, silent = true })
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -73,6 +73,9 @@ return {
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
+				menu = {
+					border = "rounded",
+				},
 				default = { "lsp", "path", "snippets", "buffer", "emoji", "sql" },
 				providers = {
 					emoji = {
