862f210d33bb8eb5673a1020a84dcd362a0675f4
Ross Pearlstone <ross@pearlstone.us>
2026-01-03 10:00:45 -0500

    treesitter changes fixed

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -67,15 +67,17 @@ return {
 			},
 
 			-- (Default) Only show the documentation popup when manually triggered
-			completion = { documentation = { auto_show = true } },
+			completion = {
+				documentation = { auto_show = true },
+				menu = {
+					border = "rounded",
+				},
+			},
 			signature = { enabled = true },
 
 			-- Default list of enabled providers defined so that you can extend it
 			-- elsewhere in your config, without redefining it, due to `opts_extend`
 			sources = {
-				menu = {
-					border = "rounded",
-				},
 				default = { "lsp", "path", "snippets", "buffer", "emoji", "sql" },
 				providers = {
 					emoji = {
